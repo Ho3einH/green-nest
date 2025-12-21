4d6b9f40bedacfeb1613ad43326d1642826299de
Ho3einH <hs.hosseini.dev@gmail.com>
2025-12-22 01:52:20 +0330

    feat(bookings): add sorting logic to booking page
    - implement sorting for bookings based on selected criteria
    - integrate sorting with existing server-side filter
    - ensure selected sort option persists and updates displayed data

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,19 +5,28 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  /* Filter */
+
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  /* Sort */
+
+  const sortByRaw = searchParams.get("sortBy") || "startDate-at";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+  console.log(sortBy);
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
