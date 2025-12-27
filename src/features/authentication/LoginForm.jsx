3d5d66e901715004164da31812322038cdd5bcad
Ho3einH <hs.hosseini.dev@gmail.com>
2025-12-27 18:38:10 +0330

    feat: implement login functionality with custom hook and API integration
    - Added `useLogin` hook for handling user login with error handling and navigation.
    - Created `login` function in `apiAuth.js` to interact with Supabase for authentication.
    - Introduced `FormRowVertical` component for consistent form layout with error display.

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,16 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import SpinnerMini from "../../ui/SpinnerMini";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, isPending } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
+      <FormRowVertical label="آدرس ایمیل">
         <Input
           type="email"
           id="email"
@@ -20,19 +27,23 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isPending}
         />
       </FormRowVertical>
-      <FormRowVertical label="Password">
+      <FormRowVertical label="رمزعبور">
         <Input
           type="password"
           id="password"
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isPending}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isPending}>
+          {isPending ? <SpinnerMini /> : "login"}
+        </Button>
       </FormRowVertical>
     </Form>
   );
