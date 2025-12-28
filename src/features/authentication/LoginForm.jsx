2afe6030b67bc5a99aa4e472f3a5177ca97d801c
Ho3einH <hs.hosseini.dev@gmail.com>
2025-12-28 16:28:32 +0330

    feat(auth): add logout functionality
    - add logout mutation
    - clear user cache on logout
    - redirect user to login page after logout

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("hossein@test.com");
+  const [password, setPassword] = useState("mohemnist");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,16 @@
+import { HiArrowLeftOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { logout, isPending } = useLogout();
+
+  return (
+    <ButtonIcon disabled={isPending} onClick={logout}>
+      {!isPending ? <HiArrowLeftOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
