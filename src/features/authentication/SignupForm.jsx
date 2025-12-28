82d518cae6de0c3c83b40b24fa01a158f18ac0a5
Ho3einH <hs.hosseini.dev@gmail.com>
2025-12-28 17:50:28 +0330

    feat(auth): add user signup form with validation
    - add signup form UI
    - handle form validation and input formats
    - display validation and error messages
    - submission logic intentionally not implemented yet

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
@@ -6,30 +7,75 @@ import Input from "../../ui/Input";
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { register, formState, getValues, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="اسم کامل" error={errors?.fullName?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", { required: "این فیلد الزامی است" })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label="آدرس ایمیل" error={errors?.email?.message}>
+        <Input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "این فیلد الزامی است",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "لطفا یک آدرس ایمیل معتبر ارائه دهید",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label="رمز عبور (حداقل 9 کاراکتر)"
+        error={errors?.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "این فیلد الزامی است",
+            minLength: {
+              value: 8,
+              message: "رمز عبور باید حداقل 8 کاراکتر داشته باشد",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label="تکرار پسورد" error={errors?.passwordConfirm?.message}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "این فیلد الزامی است",
+            validate: {
+              validate: (value) =>
+                value === getValues().password ||
+                "رمز عبور باید مطابقت داشته باشد",
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
+        <Button $variation="secondary" type="reset">
+          لغو
         </Button>
-        <Button>Create new user</Button>
+        <Button>ایجاد یک کاربر جدید</Button>
       </FormRow>
     </Form>
   );
