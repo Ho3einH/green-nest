0dc7de5cfdb7f3c37237f3a6f587db4ab65ee639
Ho3einH <hs.hosseini.dev@gmail.com>
2026-01-30 16:42:01 +0330

    chore: add supabase client with env variables for secure setup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://sosqovyhxvsonxjqimje.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvc3FvdnloeHZzb254anFpbWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NTYyODQsImV4cCI6MjA3OTAzMjI4NH0._0-v0lRWIGabiMevEb-yv4Paa3qZvO4kgq8PUcOor9k";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
